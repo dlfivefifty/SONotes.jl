9ff80a9126c35504f584d5927b8381e5fc635d77
Sheehan Olver <solver@mac.com>
2026-06-05 09:56:50 +0200

    initial commit

---
+++ src/SONotes.jl
@@ -0,0 +1,136 @@
+module SONotes
+
+using Weave
+
+export compilenotes, compilesheet, compilesheetsolution, compilelab, compilelabsolution, compilelabdemo
+
+
+#####
+# notes
+#####
+
+
+
+function replacetheorem(path, thrm, Thrm)
+    str = read(path, String)
+    str = replace(str, Regex("\\\\textbf{$Thrm \\(([^}]*)\\)}(.*?)\\\\textbf{Proof}", "s") => SubstitutionString("\\\\begin{$thrm}[\\1]\\2\\\\end{$thrm}\\n\\\\textbf{Proof}"))
+    write(path, str)
+end
+
+function replacedefinition(path, thrm, Thrm)
+    str = read(path, String)
+    str = replace(str, Regex("\\\\textbf{$Thrm \\(([^}]*)\\)}(.*?)\\\\ensuremath{\\\\QED}", "s") => SubstitutionString("\\\\begin{$thrm}[\\1]\\2\\\\end{$thrm}"))
+    write(path, str)
+end
+
+function fixwhitespace(path)
+    write(path, replace(read(path, String), "\n\n\\[" => "\n\\["))
+    write(path, replace(read(path, String), "\n\n\n\\begin{align" => "\n\\begin{align"))
+end
+
+function fixunderbars(path)
+    for ℓ in ('a':'z') ∪ ('A':'Z')
+        write(path, replace(read(path, String), "$ℓ\\ensuremath{\\underbar}" => "\\underline{$ℓ}"))
+        for bar in ("tilde", "hat", "check")
+            write(path, replace(read(path, String), "$ℓ\\ensuremath{\\$bar}" => "\\$bar{$ℓ}"))
+            write(path, replace(read(path, String), "\\ensuremath{\\bm{\\$ℓ}}\\ensuremath{\\$bar}" => "\\$bar{\\$ℓ}"))
+        end
+    end
+    
+    write(path, replace(read(path, String), "\\ensuremath{\\Sigma}\\ensuremath{\\tilde}" => "\\tilde{\\Sigma}"))
+    write(path, replace(read(path, String), "\\ensuremath{\\theta}\\ensuremath{\\tilde}" => "\\tilde{\\theta}"))
+end
+
+function compilenotes(filename)
+    weave("src/notes/$filename.jmd"; out_path="notes/", doctype="md2tex", template="src/notes/template.tpl")
+    path = "notes/$filename.tex"
+    replacetheorem(path, "theorem", "Theorem")
+    replacetheorem(path, "lemma", "Lemma")
+    replacetheorem(path, "proposition", "Proposition")
+    replacetheorem(path, "corollary", "Corollary")
+    replacedefinition(path, "example", "Example")
+    replacedefinition(path, "definition", "Definition")
+    # work around double newline before equation
+    fixwhitespace(path)
+    fixunderbars(path)
+    # work around meeq
+    write(path, replace(read(path, String), r"\\\[\n\\meeq\{(.*?)\}\n\\\]"s => s"\\meeq{\1}"))
+end
+
+function compilenotes(name)
+    weave("src/notes/$name.jmd"; out_path="notes/", doctype="md2tex", template="src/notes/notes.tpl")
+    path = "notes/$name.tex"
+    replacetheorem(path, "theorem", "Theorem")
+    replacetheorem(path, "lemma", "Lemma")
+    replacetheorem(path, "proposition", "Proposition")
+    replacetheorem(path, "corollary", "Corollary")
+    replacedefinition(path, "example", "Example")
+    replacedefinition(path, "definition", "Definition")
+    # work around double newline before equation
+    fixwhitespace(path)
+    fixunderbars(path)
+    # work around meeq
+    write(path, replace(read(path, String), r"\\\[\n\\meeq\{(.*?)\}\n\\\]"s => s"\\meeq{\1}"))
+end
+
+
+###
+# sheets
+###
+
+function compilesheet(k)
+    filename = "sheet$k"
+    write("sheets/$filename.jmd", replace(read("src/sheets/$(filename)s.jmd", String), r"\*\*SOLUTION\*\*(.*?)\*\*END\*\*"s => ""))
+    weave("sheets/$filename.jmd"; out_path="sheets/", doctype="md2tex", template="src/sheets/template.tpl")
+    path = "sheets/$filename.tex"
+    # work around double newline before equation
+    fixwhitespace(path)
+    fixunderbars(path)
+    # work around meeq
+    write(path, replace(read(path, String), r"\\\[\n\\meeq\{(.*?)\}\n\\\]"s => s"\\meeq{\1}"))
+end
+
+
+function compilesheetsolution(k)
+    filename = "sheet$k"
+    weave("src/sheets/$(filename)s.jmd"; out_path="sheets/", doctype="md2tex", template="src/sheets/template.tpl")
+    path = "sheets/$(filename)s.tex"
+    # work around double newline before equation
+    fixwhitespace(path)
+    fixunderbars(path)
+    # work around meeq
+    write(path, replace(read(path, String), r"\\\[\n\\meeq\{(.*?)\}\n\\\]"s => s"\\meeq{\1}"))
+end
+
+
+
+
+# notebook("sheets/sheet1.jmd"; pkwds...)
+# notebook("src/sheets/sheet1s.jmd"; pkwds...)
+
+
+#####
+# labs
+#####
+
+import Literate
+
+function compilelab(k)
+    str = replace(read("src/labs/lab$(k)s.jl", String), r"## SOLUTION(.*?)## END"s => "")
+    write("labs/lab$k.jl", replace(str, r"## DEMO(.*?)## END"s => s"\1"))
+    Literate.notebook("labs/lab$k.jl", "labs/"; execute=false)
+end
+
+function compilelabdemo(k)
+    str = replace(read("src/labs/lab$(k)s.jl", String), r"## SOLUTION(.*?)## END"s => "")
+    write("slides/lab$(k)d.jl", replace(str, r"## DEMO(.*?)## END"s => "##"))
+    Literate.notebook("slides/lab$(k)d.jl", "slides/"; execute=false)
+end
+
+function compilelabsolution(k)
+    str = read("src/labs/lab$(k)s.jl", String)
+    write("labs/lab$(k)s.jl", replace(str, r"## DEMO(.*?)## END"s => s"\1"))
+    Literate.notebook("labs/lab$(k)s.jl", "labs/"; execute=false)
+end
+
+end # module SONotes
